fdfa3dce88b75e24e0ab29162b9ee3ab9a0603fe
Navneet Tyagi <tyagibijnor@gmail.com>
2019-06-11 22:45:28 +0530

    test
    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
-//print_r($_POST);
-//die(testnavneet');
+print_r($_POST);
+die(testnavneet');
 if(empty($_POST['name']) || empty($_POST['email']) ||  empty($_POST['message'])	|| !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
